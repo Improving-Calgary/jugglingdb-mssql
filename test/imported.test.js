6fccd740c1f3ccbd176204798480e35ed7180dc4
Steven Eckert <stevene@quadrus.com>
2014-05-27 10:26:43 -0600

    fix tests to run against a sql server instead of memory

--- test/imported.test.js
+++ test/imported.test.js
@@ -1,9 +1,13 @@
 
 describe('mssql imported features', function() {
 
-    before(function() {
+//    if ('getSchema' in global) {
+//        global['getSchema'] = null;
+//    }
+
+//    before(function() {
         require('./init.js');
-    });
+//    });
 
     require('jugglingdb/test/common.batch.js');
     require('jugglingdb/test/include.test.js');
--- test/init.js
+++ test/init.js
@@ -1,22 +1,26 @@
 module.exports = require('should');
 
+var db = require("../db/dbconfig");
+var schemaSettings = {
+    host: db.server,
+    database: db.db,
+    username: db.user,
+    password: db.pwd,
+    options: db.options,
+    azure: db.azure
+};
+
 var Schema = require('jugglingdb').Schema;
 
 if (!('getSchema' in global)) {
-  global.getSchema = function() {
-    var db = require("../db/dbconfig");
-    var schemaSettings = {
-        host: db.server,
-        database: db.db,
-        username: db.user,
-        password: db.pwd,
-        options: db.options,
-        azure: db.azure
-    };
-    var db = new Schema(require('../'), schemaSettings);
-    db.log = function (a) {
-        console.log(a);
-    };
-    return db;
-  }
+
+    global.getSchema = function () {
+        var schema = new Schema(require('../'), schemaSettings);
+        schema.log = function (a) {
+            console.log(a);
+        };
+        return schema;
+    }
+
 }
+
